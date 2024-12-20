5d8614810dc12257127b6fe3d7909385a29d6d7f
Debasish Bordoloi <debasishbordoloi555@gmail.com>
2024-12-20 23:33:14 +0530

    got some text over the video

--- lib/components/sambodhan.dart
+++ lib/components/sambodhan.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -39,33 +40,81 @@ class _SambodhanState extends State<Sambodhan> {
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
+    final double appBarHeight = AppBar().preferredSize.height;
 
-    return FutureBuilder(
-      future: waitForInitialize,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return ClipRect(
-            // Clip the video if necessary
-            child: Container(
-              height: screenHeight,
-              width: screenWidth,
-              child: FittedBox(
-                fit: BoxFit.cover, // Fit and clip the video as needed
-                child: SizedBox(
-                  width: diamondVideoController.value.size.width,
-                  height: diamondVideoController.value.size.height,
-                  child: VideoPlayer(diamondVideoController),
+    return Stack(
+      children: [
+        FutureBuilder(
+          future: waitForInitialize,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ClipRect(
+                // Clip the video if necessary
+                child: Container(
+                  height: screenHeight - appBarHeight,
+                  width: screenWidth,
+                  child: FittedBox(
+                    fit: BoxFit.cover, // Fit and clip the video as needed
+                    child: SizedBox(
+                      width: diamondVideoController.value.size.width,
+                      height: diamondVideoController.value.size.height,
+                      child: VideoPlayer(diamondVideoController),
+                    ),
+                  ),
                 ),
+              );
+            } else {
+              return Container(
+                height: screenHeight - appBarHeight,
+                width: screenWidth,
+              );
+            }
+          },
+        ),
+        Center(
+          child: SizedBox(
+            height: screenHeight - appBarHeight - 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "Scroll",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Icon(Icons.arrow_downward),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          bottom: 0,
+          child: Container(
+            width: screenWidth,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 80),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AutoSizeText(
+                    "Hi! I am",
+                    style: TextStyle(
+                      fontSize: 54,
+                    ),
+                    maxLines: 1,
+                  ),
+                  AutoSizeText(
+                    "Debasish Bordoloi",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 72),
+                    maxLines: 1,
+                  ),
+                ],
               ),
             ),
-          );
-        } else {
-          return Container(
-            height: screenHeight,
-            width: screenWidth,
-          );
-        }
-      },
+          ),
+        ),
+      ],
     );
   }
 }
